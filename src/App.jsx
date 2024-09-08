dfae032c5b2d7a4cee9d3b0999db7346f81273de
Rafael Porras <rflporrasmen@gmail.com>
2024-09-07 21:27:19 -0600

    Creación de las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 
+import { Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Home from "./Routes/Home";
+import Detail from "./Routes/Detail";
+import Favs from "./Routes/Favs";
+import Contact from "./Routes/Contact";
 
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <>
+    <Navbar />
+    <Routes>
+      <Route path={routes.home} element={<Home />}/>
+      <Route path="/detail/:id" element={<Detail />}/>
+      <Route path={routes.favs} element={<Favs />}/>
+      <Route path={routes.contact} element={<Contact />}/>
+    </Routes>
+    <Footer/>
+    </>
   );
 }
 
